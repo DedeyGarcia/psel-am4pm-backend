41d5d44b7aab380c480373858ca04c46d169da63
Andrey Garcia <andreylucas.garcia@gmail.com>
2026-06-11 14:58:41 -0300

    fix: swagger not sending api key in get me request

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -5,6 +5,7 @@ import { CreateUsuarioDto } from './dto/create-usuario.dto';
 import { Usuario } from './entities/usuario.entity';
 import { Public } from '../auth/public.decorator';
 import { type AuthUser, CurrentUser } from '../auth/current-user.decorator';
+import { ApiAuth } from '../auth/api-auth.decorator';
 
 @Controller('usuarios')
 export class UsuariosController {
@@ -18,6 +19,7 @@ export class UsuariosController {
     return user;
   }
 
+  @ApiAuth()
   @Get('me')
   @ApiOkResponse({ type: Usuario })
   async findOne(@CurrentUser() currentUser: AuthUser): Promise<Usuario> {
